36378bd432d0e44c4b0ef722f1fc447934554b0a
DinhTrungQuy <quythks2023@gmail.com>
2024-02-20 10:23:50 +0700

    feat: add cart, order_item model

---
+++ lib/model/cart.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+import 'package:travelapp/model/order_item.dart';
+
+class Cart with ChangeNotifier {
+  List<OrderItem> itemsInCart = [];
+
+  double get totalCost {
+    double total = 0;
+    for (var item in itemsInCart) {
+      total += item.place.price;
+    }
+    return total;
+  }
+
+  void add(OrderItem orderItem) {
+    itemsInCart.add(orderItem);
+    notifyListeners();
+  }
+
+  void remove(OrderItem orderItem) {
+    // print(orderItem.product.name);
+    itemsInCart.remove(orderItem);
+    // print(t);
+    notifyListeners();
+  }
+}
---
+++ lib/model/order_item.dart
@@ -0,0 +1,13 @@
+import 'package:travelapp/model/place.dart';
+
+class OrderItem {
+  Place place;
+
+  /// Selected size of place; This can be null
+  String? selectedSize;
+
+  /// Selected color of place; This can be null
+  String? selectedColor;
+
+  OrderItem({required this.place, this.selectedSize, this.selectedColor});
+}
